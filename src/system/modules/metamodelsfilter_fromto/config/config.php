9c4a9745c9f55d65b2cb00f1d369bca2226a3890
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-02-07 17:20:11 +0100

    Moving to version 2.

--- src/system/modules/metamodelsfilter_fromto/config/config.php
+++ src/system/modules/metamodelsfilter_fromto/config/config.php
@@ -20,10 +20,6 @@
  */
 $GLOBALS['METAMODELS']['filters']['fromto']['class'] = 'MetaModels\Filter\Setting\FromTo';
 $GLOBALS['METAMODELS']['filters']['fromto']['image'] = 'system/modules/metamodelsfilter_fromto/html/filter_fromto.png';
-$GLOBALS['METAMODELS']['filters']['fromto']['info_callback'] = array('MetaModels\Dca\Filter', 'infoCallback');
+$GLOBALS['METAMODELS']['filters']['fromto']['info_callback'] = array('MetaModels\DcGeneral\Events\Table\FilterSetting\DrawSetting', 'modelToLabelWithAttributeAndUrlParam');
 $GLOBALS['METAMODELS']['filters']['fromto']['attr_filter'][] = 'numeric';
 $GLOBALS['METAMODELS']['filters']['fromto']['attr_filter'][] = 'decimal';
-
-// non composerized Contao 2.X autoload support.
-$GLOBALS['MM_AUTOLOAD'][] = dirname(__DIR__);
-$GLOBALS['MM_AUTOLOAD'][] = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'deprecated';
