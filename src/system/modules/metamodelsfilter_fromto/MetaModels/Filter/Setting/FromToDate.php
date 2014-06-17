89fa7bf1c40419d43e92ad1ca39cc5bf6bc77483
Stefan Heimes <stefan_heimes@hotmail.com>
2014-06-17 22:09:17 +0200

    Add the "getParameterFilterWidgets" function. Add some more information to the array for the template. Now the format and type are pass to the template.

--- src/system/modules/metamodelsfilter_fromto/MetaModels/Filter/Setting/FromToDate.php
+++ src/system/modules/metamodelsfilter_fromto/MetaModels/Filter/Setting/FromToDate.php
@@ -161,4 +161,106 @@ class FromToDate extends FromTo
 		// Make a unix timestamp from the string.
 		return $date;
 	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getParameterFilterWidgets(
+		$arrIds,
+		$arrFilterUrl,
+		$arrJumpTo,
+		FrontendFilterOptions $objFrontendFilterOptions
+	)
+	{
+		$objAttribute = $this->getMetaModel()->getAttributeById($this->get('attr_id'));
+
+		$arrOptions = $objAttribute->getFilterOptions(
+			($this->get('onlypossible') ? $arrIds : null),
+			(bool)$this->get('onlyused')
+		);
+
+		// Remove empty values from list.
+		foreach ($arrOptions as $mixKeyOption => $mixOption)
+		{
+			// Remove html/php tags.
+			$mixOption = strip_tags($mixOption);
+			$mixOption = trim($mixOption);
+
+			if ($mixOption === '' || $mixOption === null)
+			{
+				unset($arrOptions[$mixKeyOption]);
+			}
+		}
+
+		$arrLabel = array(
+			($this->get('label') ? $this->get('label') : $objAttribute->getName()),
+			'GET: '.$this->get('urlparam')
+		);
+
+		if ($this->get('fromfield') && $this->get('tofield'))
+		{
+			$arrLabel[0] .= ' '.$GLOBALS['TL_LANG']['metamodels_frontendfilter']['fromto'];
+		}
+		elseif($this->get('fromfield') && !$this->get('tofield'))
+		{
+			$arrLabel[0] .= ' '.$GLOBALS['TL_LANG']['metamodels_frontendfilter']['from'];
+		}
+		else
+		{
+			$arrLabel[0] .= ' '.$GLOBALS['TL_LANG']['metamodels_frontendfilter']['to'];
+		}
+
+		$arrUrlValue = $arrFilterUrl[$this->getParamName()];
+
+		// Split up our param so the widgets can use it again.
+		$strParamName   = $this->getParamName();
+		$arrMyFilterUrl = $arrFilterUrl;
+		// If we have a value, we have to explode it by double underscore to have a valid value which the active checks
+		// may cope with.
+		if (array_key_exists($strParamName, $arrFilterUrl) && !empty($arrFilterUrl[$strParamName]))
+		{
+			if (is_array($arrFilterUrl[$strParamName]))
+			{
+				$arrParamValue = $arrFilterUrl[$strParamName];
+			} else {
+				$arrParamValue = explode('__', $arrFilterUrl[$strParamName], 2);
+			}
+
+			if ($arrParamValue && ($arrParamValue[0] || $arrParamValue[1]))
+			{
+				$arrMyFilterUrl[$strParamName] = $arrParamValue;
+			} else {
+				// No values given, clear the array.
+				$arrParamValue = null;
+			}
+		}
+
+		$GLOBALS['MM_FILTER_PARAMS'][] = $this->getParamName();
+
+		return array(
+			$this->getParamName() => $this->prepareFrontendFilterWidget(
+					array
+					(
+						'label'         => $arrLabel,
+						'inputType'     => 'multitext',
+						'options'       => $arrOptions,
+						'timetype'      => $this->get('timetype'),
+						'dateformat'    => $this->get('dateformat'),
+						'eval'          => array
+						(
+							'multiple'  => true,
+							'size'      => ($this->get('fromfield') && $this->get('tofield') ? 2 : 1),
+							'urlparam'  => $this->get('urlparam'),
+							'template'  => $this->get('template'),
+						),
+						// We need to implode to have it transported correctly in the frontend filter.
+						'urlvalue'      => !empty($arrParamValue) ? implode('__', $arrParamValue) : ''
+					),
+					$arrMyFilterUrl,
+					$arrJumpTo,
+					$objFrontendFilterOptions
+				)
+		);
+	}
+
 }
