c3b20ee94d74ba5c3e4b89c93cd2650f75b8d3f7
Mini Model <minimodel@metamodel.me>
2015-09-18 12:15:39 +0200

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_filtersetting.php
+++ contao/languages/de/tl_metamodel_filtersetting.php
@@ -8,9 +8,11 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T07:34:45+02:00
+ * last-updated: 2015-07-11T08:02:20+02:00
  */
 
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['dateformat']['0']          = 'Datumsformat';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['dateformat']['1']          = 'Sie können eine Formatierung für das Datum hinzufügen. Mit PHP-Funktionen ist es möglich, aus einem Unix-Zeitstempel eine Datumsausgabe umzuwandeln.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fromfield']['0']           = 'Feld für Wert 1';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fromfield']['1']           = 'Zeige Frontend-Feld für Wert 1.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['0']           = 'Wert 2 einfügen';
@@ -21,7 +23,9 @@ $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetype']['0']            = 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetype']['1']            = 'Hier können die das gewünschte Schema auswählen.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['date']  = 'Datum';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['datim'] = 'Datum und Uhrzeit';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['time']  = 'Zeit';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['0']             = 'Feld für Wert 2';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['1']             = 'Zeige Frontend-Feld für Wert 2.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['fromto']      = 'Wert von/bis';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['fromtodate']  = 'Wert von/bis für ein Datum';
 
