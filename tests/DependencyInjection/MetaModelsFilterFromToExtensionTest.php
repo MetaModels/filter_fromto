c6800999378f6ef26324218edeb1063540545900
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-12-27 12:54:33 +0100

    Add tests

---
+++ tests/DependencyInjection/MetaModelsFilterFromToExtensionTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * * This file is part of MetaModels/filter_fromto.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage FilterFromToBundle
+ * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/filter_fromto/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\FilterFromToBundle\Test\DependencyInjection;
+
+use MetaModels\FilterFromToBundle\FilterSetting\FromToFilterSettingTypeFactory;
+use MetaModels\FromToFilterBundle\DependencyInjection\MetaModelsFilterFromToExtension;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * This test case test the extension.
+ */
+class MetaModelsFilterFromToExtensionTest extends TestCase
+{
+    /**
+     * Test that extension can be instantiated.
+     *
+     * @return void
+     */
+    public function testInstantiation()
+    {
+        $extension = new MetaModelsFilterFromToExtension();
+
+        $this->assertInstanceOf(MetaModelsFilterFromToExtension::class, $extension);
+        $this->assertInstanceOf(ExtensionInterface::class, $extension);
+    }
+
+    /**
+     * Test that the services are loaded.
+     *
+     * @return void
+     */
+    public function testFactoryIsRegistered()
+    {
+        $container = $this->getMockBuilder(ContainerBuilder::class)->getMock();
+
+        $container
+            ->expects($this->atLeastOnce())
+            ->method('setDefinition')
+            ->withConsecutive(
+                [
+                    'metamodels.filter_fromto.factory',
+                    $this->callback(
+                        function ($value) {
+                            /** @var Definition $value */
+                            $this->assertInstanceOf(Definition::class, $value);
+                            $this->assertEquals(FromToFilterSettingTypeFactory::class, $value->getClass());
+                            $this->assertCount(1, $value->getTag('metamodels.filter_factory'));
+
+                            return true;
+                        }
+                    )
+                ]
+            );
+
+        $extension = new MetaModelsFilterFromToExtension();
+        $extension->load([], $container);
+    }
+}
