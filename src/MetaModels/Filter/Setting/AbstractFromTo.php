MetaModels/filter_fromto

commit b8198e41766c18757fb84bfa8a1f796acbe5af37

Add better support for formats of the date.
This improve the handling of the filter isn't able to translate the string into a valid date.

src/MetaModels/Filter/Setting/AbstractFromTo.php
@@ -301,44 +301,54 @@ abstract class AbstractFromTo extends Simple
             return;
         }
 
+        // No attribute, get out.
         $attribute = $this->getMetaModel()->getAttributeById($this->get('attr_id'));
-
         if (!($attribute)) {
             $objFilter->addFilterRule(new StaticIdList(null));
 
             return;
         }
 
-        $value = $this->getParameterValue($arrFilterUrl);
         // No filter values, get out.
+        $value = $this->getParameterValue($arrFilterUrl);
         if (empty($value)) {
             $objFilter->addFilterRule(new StaticIdList(null));
 
             return;
         }
 
+        // Preinit some vars and than check the format.
+        $formattedValueZero = null;
+        $formattedValueOne  = null;
+
+        // Two values, apply filtering for a value range if both fields are allowed.
+        if(count($value) == 2 && !($this->get('fromfield') && $this->get('tofield'))){
+            throw new \LengthException('Only one value is allowed, please configure fromfield and tofield.');
+        } else if(count($value) == 2){
+            $formattedValueZero = $this->formatValue($value[0]);
+            $formattedValueOne  = $this->formatValue($value[1]);
+        } else {
+            $formattedValueZero = $this->formatValue($value[0]);
+        }
+
+        // If something went wrong return an empty list.
+        if ($formattedValueZero === false || $formattedValueOne === false) {
+            $objFilter->addFilterRule(new StaticIdList([]));
+
+            return;
+        }
+
+        // Add rule to the filter.
         $rule = $this->buildFromToRule($attribute);
         $objFilter->addFilterRule($rule);
 
         if (count($value) == 2) {
-            // Two values, apply filtering for a value range if both fields are allowed.
-            if (!($this->get('fromfield') && $this->get('tofield'))) {
-                throw new \LengthException('Only one value is allowed, please configure fromfield and tofield.');
-            }
-
-            $rule
-                ->setLowerBound($this->formatValue($value[0]), $this->get('moreequal'))
-                ->setUpperBound($this->formatValue($value[1]), $this->get('lessequal'));
-
-            return;
+            $rule->setLowerBound($formattedValueZero, $this->get('moreequal'))
+                 ->setUpperBound($formattedValueOne, $this->get('lessequal'));
+        } else if ($this->get('fromfield')) { // Only one value, decide which one to use.
+            $rule->setLowerBound($formattedValueZero, $this->get('moreequal'));
+        } else {
+            $rule->setUpperBound($formattedValueZero, $this->get('lessequal'));
         }
-
-        // Only one value, decide which one to use.
-        if ($this->get('fromfield')) {
-            $rule->setLowerBound($this->formatValue($value[0]), $this->get('moreequal'));
-
-            return;
-        }
-        $rule->setUpperBound($this->formatValue($value[0]), $this->get('lessequal'));
     }
 }

commit 0ac67f59e43edc37ebdf197abf9b7f6cf9b68e15

Restore functionality to have an open start
It got broken in b8198e4 and 255cd41.

Now we only return empty results if the value can not be parsed and not
if it contains an empty string.

src/MetaModels/Filter/Setting/AbstractFromTo.php
@@ -37,7 +37,7 @@ abstract class AbstractFromTo extends Simple
      *
      * @param mixed $value The value to format.
      *
-     * @return string
+     * @return string|bool
      */
     abstract protected function formatValue($value);
 
@@ -321,35 +321,48 @@ abstract class AbstractFromTo extends Simple
             return;
         }
 
-        // Preinit some vars and than check the format.
-        $formattedValueZero = null;
-        $formattedValueOne  = null;
-
         // Two values, apply filtering for a value range if both fields are allowed.
-        if (count($value) == 2 && !($this->get('fromfield') && $this->get('tofield'))) {
-            throw new \LengthException('Only one value is allowed, please configure fromfield and tofield.');
-        } elseif (count($value) == 2) {
-            $formattedValueZero = $this->formatValue($value[0]);
-            $formattedValueOne  = $this->formatValue($value[1]);
-        } else {
-            $formattedValueZero = $this->formatValue($value[0]);
+        if (count($value) == 2) {
+            if (!($this->get('fromfield') && $this->get('tofield'))) {
+                throw new \LengthException('Only one value is allowed, please configure fromfield and tofield.');
+            }
+
+            // Add rule to the filter.
+            $objFilter->addFilterRule(
+                $this->createFromToRule($attribute, $this->formatEmpty($value[0]), $this->formatEmpty($value[1]))
+            );
         }
 
         // Add rule to the filter.
-        $objFilter->addFilterRule($this->createFromToRule($attribute, $value, $formattedValueZero, $formattedValueOne));
+        $objFilter->addFilterRule($this->createFromToRule($attribute, $this->formatEmpty($value[0]), null));
+    }
+
+    /**
+     * Format the value but return empty if it is empty.
+     *
+     * @param string $value The value to format.
+     *
+     * @return bool|string
+     */
+    private function formatEmpty($value)
+    {
+        if (empty($value = trim($value))) {
+            return $value;
+        }
+
+        return $this->formatValue($value);
     }
 
     /**
      * Create and populate a rule instance.
      *
      * @param IAttribute $attribute          The attribute to filter on.
-     * @param mixed      $value              The value to filter for.
      * @param string     $formattedValueZero The formatted first value.
      * @param string     $formattedValueOne  The formatted second value.
      *
      * @return FromTo|StaticIdList
      */
-    private function createFromToRule(IAttribute $attribute, $value, $formattedValueZero, $formattedValueOne)
+    private function createFromToRule(IAttribute $attribute, $formattedValueZero, $formattedValueOne)
     {
         // If something went wrong return an empty list.
         if ($formattedValueZero === false || $formattedValueOne === false) {
@@ -359,7 +372,7 @@ abstract class AbstractFromTo extends Simple
         // Add rule to the filter.
         $rule = $this->buildFromToRule($attribute);
 
-        if (count($value) == 2) {
+        if (null !== $formattedValueOne) {
             $rule->setLowerBound($formattedValueZero, $this->get('moreequal'))
                 ->setUpperBound($formattedValueOne, $this->get('lessequal'));
             return $rule;

tests/MetaModels/Test/Filter/Setting/FromToDateTest.php
@@ -199,14 +199,14 @@ class FromToDateTest extends FromToTestCase
                 'filterSetting' => $baseSettings,
                 'data'          => $baseData,
                 'filterValues'  => array('urlParameter' => $generateUrlValue('broken', $baseData[4])),
-                'expected'      => array(1, 2, 3),
+                'expected'      => array(),
                 'message'       => 'filtering exclusive with broken start but valid end.'
             ),
             14 => array(
                 'filterSetting' => $baseSettings,
                 'data'          => $baseData,
                 'filterValues'  => array('urlParameter' => $generateUrlValue($baseData[4], 'broken')),
-                'expected'      => array(5, 6),
+                'expected'      => array(),
                 'message'       => 'filtering exclusive with valid start but broken end.'
             ),
             16 => array(